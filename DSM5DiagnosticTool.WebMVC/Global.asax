3929c5652cc623093fcce089622b11070c2b21e3
Duncan Summers <djsummers36@gmail.com>
2021-10-05 09:55:27 -0400

    data, models, and services libraries added; added gitignore file

---
+++ DSM5DiagnosticTool.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DSM5DiagnosticTool.WebMVC.MvcApplication" Language="C#" %>
---
+++ DSM5DiagnosticTool.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DSM5DiagnosticTool.WebMVC.Startup))]
+namespace DSM5DiagnosticTool.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
