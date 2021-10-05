7eae346ae830e98a387cfca549fb3559428f8d77
Duncan Summers <djsummers36@gmail.com>
2021-10-05 11:48:52 -0400

    added rough drafts of data layer and foreign keys

---
+++ DSM5.Data/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSM5.Data
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        [Required]
+        public string[] Subcategories { get; set; }
+    }
+}
