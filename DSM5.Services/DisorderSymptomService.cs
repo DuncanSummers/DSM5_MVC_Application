394ea6f569b525b072dc59bc491585d54e6c9b7e
Duncan Summers <djsummers36@gmail.com>
2021-10-14 10:52:17 -0400

    minor changes made

--- DSM5.Services/DisorderSymptomService.cs
+++ DSM5.Services/DisorderSymptomService.cs
@@ -75,8 +75,8 @@ namespace DSM5.Services
                         .DisorderSymptoms
                         .Single(e => e.DisorderID == model.DisorderID);
 
-                entity.DisorderID = model.DisorderID;
                 entity.SymptomID = model.SymptomID;
+                entity.DisorderID = model.DisorderID;
 
                 return ctx.SaveChanges() == 1;
             }
